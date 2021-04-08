4b543e685621d3c44238a282094dddcc7b639b7d
themloc987654321 <loc.huynh@namlongsoft.net>
2021-04-09 02:03:10 +0700

    dont under

--- src/components/form/DropdownConponents.js
+++ src/components/form/DropdownConponents.js
@@ -2,23 +2,21 @@ import React, { useRef, useState } from 'react'
 import LabelComponents from './LabelComponents';
 // import PropTypes from 'prop-types'
 
-const DropdownConponents = ({ title, classLabel, ...props }) => {
-
-  // const [itemmmm, setitemmmm] = useState({ name: "CPN" });
+const DropdownConponents = ({ title, classLabel }) => {
 
   const [isActive, setIsActive] = useState(false);
   const onClick = () => setIsActive(!isActive);
 
-  const [items,setItems] = React.useState([
+  const items = [
     { label: "Luke Skywalker", value: "Luke Skywalker" },
     { label: "C-3PO", value: "C-3PO" },
     { label: "R2-D2", value: "R2-D2" }
-  ]);
+  ];
 
-  const handleChange = (e) => {
-    const selected = e.target.value;
-      setItems(selected);
-  }
+  const listItems = items.map((item) =>
+    <li>{item.value}</li>
+  );
+
   
 
   return (
@@ -29,16 +27,12 @@ const DropdownConponents = ({ title, classLabel, ...props }) => {
             {title}
           </span>
         }
-        <div onclick="dropdownFunc(this)" className="dropbtn btn-default selection" onChange = {handleChange}>
-          {item.value}
+        <div className="dropbtn btn-default selection" >
+          
         </div>
         <div className={`dropdown-menu ${isActive ? 'active' : 'inactive'}`}>
           <ul>
-            {items.map(item => (
-              <li className="items">
-                {item.value}
-              </li>
-            ))}
+              {listItems}
           </ul>
         </div>
 
